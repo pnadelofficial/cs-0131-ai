b9fc5d4d5d50efda24973356a5d1201453d421ff
peter nadel <peter.m.nadel@gmail.com>
2025-06-06 13:51:38 -0400

    dusty spot update

--- bt_complete_pn/bt_py_2.0.1/bt/conditions/dusty_spot.py
+++ bt_complete_pn/bt_py_2.0.1/bt/conditions/dusty_spot.py
@@ -1,5 +1,7 @@
 import bt_library as btl
-from ..globals import DUSTY_SPOT_SENSOR
+from ..globals import DUSTY_SPOT_SENSOR, CURRENT_COORDINATES
+import random
+import turtle
 
 class DustySpot(btl.Condition):
     """
@@ -9,6 +11,20 @@ class DustySpot(btl.Condition):
         self.print_message("Checking a dusty spot")
 
         if blackboard.get_in_environment(DUSTY_SPOT_SENSOR, False):
+            self.print_message("Dusty spot detected!")
+            # Create random dusty spot
+            dusty_spot = (random.randint(-100, 100), random.randint(-100, 100))
+
+            # Change color
+            turtle.color("blue")
+            # Move to dusty spot
+            self.print_message(f"Moving to dusty spot at X={dusty_spot[0]} and Y={dusty_spot[1]}")
+            turtle.setx(dusty_spot[0])
+            turtle.sety(dusty_spot[1])
+            
+            # Update current coordinates in the blackboard
+            blackboard.set_in_environment(CURRENT_COORDINATES, dusty_spot)
+
             return self.report_succeeded(blackboard)
         else:
             return self.report_failed(blackboard)
