4f0eba1512373d926454e4a4b45c5333b50f8332
pnadelofficial <75741790+pnadelofficial@users.noreply.github.com>
2025-06-18 19:05:50 +0000

    final base

--- informed-search/pancake_problem.py
+++ informed-search/pancake_problem.py
@@ -22,13 +22,13 @@ def gap_heuristic(state:Tuple) -> int:
 
 def no_heuristic(state:Tuple) -> int:
     """
-    Uniform Cost Search heuristic -- a heuristic that always returns 0
+    Uniform Cost Search heuristic -- a heuristic that always returns None
     Args:
         state (tuple): Order of pancakes
     Returns:
-        int: Always returns zero as the UCS algorithm ignores heuristics
+        int: Always returns None as the UCS algorithm ignores heuristics
     """
-    return 0
+    return None
 
 class PancakeStack:
     """
@@ -63,7 +63,7 @@ class PancakeStack:
         my_total_cost = self.total_cost()
         others_total_cost = other.total_cost()
         if my_total_cost == others_total_cost:
-            return self.added > other.added
+            return self.added > other.added # using the added to break ties: stacks added later will be given preference
         return my_total_cost < others_total_cost
 
     def total_cost(self) -> int:
@@ -72,7 +72,11 @@ class PancakeStack:
         Returns:
             int: Total cost of a stack, the sum of the heuristic given the state and the backward cost
         """
-        return self.heuristic(self.state) + self.b_cost
+        h = self.heuristic(self.state) 
+        if h: # check if h returns None
+            return self.heuristic(self.state) + self.b_cost
+        else:
+            return self.b_cost
 
     def flip(self, k) -> Self:
         """
@@ -82,7 +86,7 @@ class PancakeStack:
         Returns:
             PancakeStack: New pancake stack made up for the flipped state with a new added and backward cost
         """
-        new_state = self.state[:k+1][::-1] + self.state[k+1:]
+        new_state = self.state[:k+1][::-1] + self.state[k+1:] # pythonic way to do flip: cuts the stack at k+1, reverses everything before and concatenates it to the rest of the original stack
         new_b_cost = self.b_cost + 1 # increment backward cost
         return PancakeStack(
             state=new_state,
@@ -130,7 +134,7 @@ class PriorityQueue:
             return item
         return None # return none if we only have obsolete nodes -- this shouldn't happen except in degenerate cases of the problem as a stack is only marked obsolete if there is another node that costs less than it with the same state.
 
-    def is_empty(self):
+    def is_empty(self) -> bool:
         """
         Checks if the pq is empty which would trigger an end to the A* algorithm
         Returns:
@@ -138,7 +142,7 @@ class PriorityQueue:
         """
         return len(self.pq) == 0
 
-    def get_node_with_state(self, state):
+    def get_node_with_state(self, state:Tuple) -> PancakeStack:
         """
         Retrieves a node given a state
         Args:
@@ -148,7 +152,7 @@ class PriorityQueue:
         """
         return self.state2node.get(state, None)
 
-    def has(self, state):
+    def has(self, state) -> bool:
         """
         Checks whether a state is already in the pq
         Args:
@@ -159,56 +163,81 @@ class PriorityQueue:
         return state in self.state2node
 
 class AStar:
-    def __init__(self, initial_state, heuristic=gap_heuristic):
+    """
+    Implementation of the A* algorithm to solve the Pancake Problem
+    """
+    def __init__(self, initial_state:Tuple, heuristic:callable=gap_heuristic) -> None:
+        """
+        Initializes the A* object for searching the given initial stack
+        Args:
+            initial_state (tuple): the initial state of the stack, the starting point of the pancake problem
+            heuristic (callable): the heuristic to use when solving the problem 
+        """
         self.initial_state = initial_state
         self.length = len(initial_state)
         self.heuristic = heuristic
 
         self.solution = False
-        self.visited = set()
+        self.visited = set() # set of all visited nodes to avoid visiting the same node twice
         self.pq = PriorityQueue()
 
-        self.root = PancakeStack(initial_state, None, 0, 0, heuristic)
+        self.root = PancakeStack(initial_state, None, 0, 0, heuristic) # initial PancakeStack
         self.pq.push(self.root)
-        self.added = 1
+        self.added = 1 # set added to 1 
     
-    def search(self):
+    def search(self) -> None:
         while True:
-            if self.pq.is_empty():
+            # print(self.pq.pq)
+            if self.pq.is_empty(): # if pq is empty then we end
                 return
         
             current = self.pq.pop()
-            if current is None: 
+            if current is None: # if pq pops a None then we end (see pop for more explanation)
                 return
             
-            self.visited.add(current.state) 
+            self.visited.add(current.state) # add current state to visited
 
-            if current.state == GOLD:
-                self.solution = current
+            if current.heuristic(current.state) is None: # in ucs, need to check if we have any gaps wit hthe gap heuristic, used now as a check
+                gaps = gap_heuristic(current.state)
+                if gaps == 0:
+                    self.solution = current
+                    return    
+            
+            if current.heuristic(current.state) == 0:                
+                if current.state[0] < current.state[-1]: # if it is lower first
+                    current = current.flip(len(current.state)+1) # flip it at the last position
+                self.solution = current # set solution to the current stack
                 return
         
-            self.generate_successors(current)
+            self.generate_successors(current) # expand frontier
     
-    def generate_successors(self, current):
-        for i in range(2, self.length+1):
+    def generate_successors(self, current:PancakeStack) -> None:
+        for i in range(2, self.length+1): # iterate across all of the flips which would change the stack
             successor = current.flip(i)
             
-            if successor.state in self.visited:
+            if successor.state in self.visited: # pass if visited the flipped stack
                 continue
         
-            if not self.pq.has(successor.state):
+            if not self.pq.has(successor.state): # push if we haven't visited the stack and the stack is not in the pq
                 self.pq.push(successor)
             else:
+                # if it is in the pq then we have some options:
+                # the existing stack either costs more or less than the exisiting stack 
+                # if it cost more we can move on
                 possible_existing_stack = self.pq.get_node_with_state(successor.state)
-                if possible_existing_stack and (successor < possible_existing_stack):
-                    possible_existing_stack.obsolete = True
-                    self.pq.push(successor)
+                if possible_existing_stack and (successor < possible_existing_stack): # if it costs less
+                    possible_existing_stack.obsolete = True # then we mark the existing stack as obsolete
+                    self.pq.push(successor) # and push the newly popped stack
 
-    def print_results(self):
+    def print_results(self) -> None:
         results = []
         
         current = self.solution
-        while current.parent:
+        if not current:
+            print("No solution found.")
+            return
+
+        while current.parent: # backtrack to find parents
             results.append(current.state)
             current = current.parent
         
@@ -224,20 +253,24 @@ def main():
     stack = args.stack
     if stack == []:
         stack = [i for i in range(1, 11)]
-        random.shuffle(stack)
-        print(f"Initial stack: {stack}")
+        random.shuffle(stack) # if no stack is given we randomly create our own
+    stack = tuple(stack)
 
     if args.ucs:
         astar = AStar(stack, heuristic=no_heuristic)
     else:
         astar = AStar(stack, heuristic=gap_heuristic)
-    
+
+    print(f"Initial stack: {stack}")
+
     cur = time.time()
     astar.search()
-    astar.print_results()
     elapsed = time.time() - cur
 
-    print(f"Pancake Problem sovled in {round(elapsed, 4)} seconds")
+    astar.print_results()
+    
+    print(f"Ran for {round(elapsed, 4)} seconds | Visited {len(astar.visited)} nodes")
+    return elapsed, len(astar.visited)
 
 if __name__ == '__main__':
     main()
